20acafe1158d5d00aa20bc900ce0aa4b22379f04
Bruce Kim <mannypac12@gmail.com>
2018-11-20 21:00:26 +0900

    Spiral is Really Hard!

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -9,12 +9,61 @@
 //     [[1, 2, 3],
 //     [8, 9, 4],
 //     [7, 6, 5]]
+// 00 01 02 12 22 
 //  matrix(4)
 //     [[1,   2,  3, 4],
 //     [12, 13, 14, 5],
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
+// 00 01 02 03 13 23 33 32 31 30 20 10 11 12 22 21
 
-function matrix(n) {}
+
+function matrix(n) {
+
+    const results = [];
+    for (let i = 0; i < n; i++) {
+        results.push([]);
+        console.log(results)
+    }
+    
+    let counter = 1;
+    let startColumn = 0;
+    let endColumn = n-1;
+    let startRow = 0;
+    let endRow = n-1;
+
+    while (startColumn <= endColumn && startRow <= endRow) {
+        for (let i = startColumn; i <= endColumn; i++) {
+            results[startRow][i] = counter;
+            counter++;
+        }
+        startRow++;
+
+        for (let i = startRow; i<= endRow; i++) {
+            results[i][endColumn] = counter;
+            counter++ 
+        }
+        endColumn--;
+
+        for (let i = endColumn; i >= startColumn; i--) {
+            results[endRow][i] = counter;
+            counter++;
+        }
+        endRow--;
+
+        for (let i = endRow; i >= startRow; i--) {
+            results[i][startColumn] = counter;
+            counter++
+        }
+        startColumn++;
+    }
+
+    return results
+    // n nested Array
+    // n elements for each nested array
+    // elements should be incremented
+}
+
+console.log(matrix(4))
 
 module.exports = matrix;
